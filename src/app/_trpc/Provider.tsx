1abf3b941afecef984ccdc224e142f799bacc125
iñaki sorovigarat <inakisorovigarat@Mac.home>
2025-05-14 12:03:51 +0200

    fix(Provider) fixed cors to build

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -1,24 +1,44 @@
 "use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+	QueryClient,
+	QueryClientProvider,
+} from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import React, { useState } from "react";
 
 import { trpc } from "./client";
 
-export default function Provider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient({}));
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [
-        httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
-        }),
-      ],
-    })
-  );
-  return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </trpc.Provider>
-  );
+const getBaseUrl = () => {
+	if (typeof window !== "undefined") return "";
+	if (process.env.VERCEL_URL)
+		return `https://${process.env.VERCEL_URL}`;
+	return "http://localhost:3000";
+};
+
+export default function Provider({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	const [queryClient] = useState(() => new QueryClient({}));
+	const [trpcClient] = useState(() =>
+		trpc.createClient({
+			links: [
+				httpBatchLink({
+					url: `${getBaseUrl()}/api/trpc`,
+				}),
+			],
+		})
+	);
+
+	return (
+		<trpc.Provider
+			client={trpcClient}
+			queryClient={queryClient}
+		>
+			<QueryClientProvider client={queryClient}>
+				{children}
+			</QueryClientProvider>
+		</trpc.Provider>
+	);
 }
